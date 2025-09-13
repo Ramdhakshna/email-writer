de49957489664424f1b65fda981d9956454e5c56
Dhakshna Moorthy <101925093+Ramdhakshna@users.noreply.github.com>
2025-09-13 07:22:23 +0530

    test

--- email-writer/src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ email-writer/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -1,5 +1,7 @@
 package com.email.writer.app;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -16,8 +18,8 @@ public class EmailGeneratorService {
     @Value("${gemini.api.key}")
     private String geminiApiKey;
 
-    public EmailGeneratorService(WebClient webClient) {
-        this.webClient = webClient;
+    public EmailGeneratorService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
     }
 
 
@@ -39,13 +41,30 @@ public class EmailGeneratorService {
         String response = webClient.post()
                 .uri(geminiApiUrl+ geminiApiKey)
                 .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
 
-        // Return response
+        // Extract response and return
+        return extractResponseContent(response);
     }
 
+    private String extractResponseContent(String response){
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(response);
+            return rootNode.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+        } catch (Exception e) {
+            return "Error processing request: " + e.getMessage();
+        }
+    }
     private String buildPrompt(EmailRequest emailRequest){
         StringBuilder prompt = new StringBuilder();
         prompt.append("Generate a professional email reply for the following email content. Please don't generate a subject line ");
